ee8a712ffc758e9fc53f6d961e83b491ebe12af2
seong-94 <togussla@naver.com>
2023-12-24 02:43:36 +0900

    feat: dd 태그 변경

--- content-script.js
+++ content-script.js
@@ -5,11 +5,14 @@
 //     elements.forEach((element) => element.)
 // }
 // .chat_area dl dd
-function editCharAreaPadding(selector) {
-  const elements = document.querySelectorAll(selector);
-  elements.forEach((element) => (element.style.padding = "1px 0 1px 10px"));
+function editCharAreaPadding(selectors) {
+  selectors.forEach((selector) => {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach((element) => {
+      element.style.padding = "1px 0 1px 10px";
+    });
+  });
 }
-
 function editCharAreaFlex(selector) {
   const elements = document.querySelectorAll(selector);
   elements.forEach((element) => (element.style.display = "flex"));
@@ -23,17 +26,71 @@ function removeElementBySelector(selector) {
   const elements = document.querySelectorAll(selector);
   elements.forEach((element) => element.remove());
 }
+
+// 함수: dl 태그 내의 dt의 텍스트를 dd 내부에 span으로 랜더링
+function moveDdTextToDt(dlSelector) {
+  // dlSelector에 해당하는 모든 dl 요소를 찾습니다.
+  const dlElements = document.querySelectorAll(dlSelector);
+
+  // 각 dl 요소에 대해 반복 처리합니다.
+  dlElements.forEach((dl) => {
+    const dtElement = dl.querySelector("dt");
+    const ddElement = dl.querySelector("dd");
+
+    // dd 요소가 존재하면 작업을 수행합니다.
+    if (ddElement) {
+      // dd의 텍스트를 취득합니다.
+      const text = ddElement.textContent || ddElement.innerText;
+
+      // 새로운 p 요소를 생성하고 텍스트를 설정합니다.
+      const p = document.createElement("p");
+      p.textContent = text;
+      p.style.color = "#fff";
+      p.style.display = "inline";
+      p.style.padding = "1px 0 1px 5px";
+
+      // dt 요소가 존재하면, 새로운 p 요소를 dt에 추가합니다.
+      if (dtElement) {
+        dtElement.appendChild(p);
+      }
+
+      // 기존의 dd 요소를 제거합니다.
+      ddElement.remove();
+    }
+  });
+}
+
+// function getRandomPastelColor() {
+//   // 파스텔 색상을 위해 높은 RGB 값을 랜덤으로 생성
+//   const r = Math.floor(Math.random() * 128 + 127); // 127-255
+//   const g = Math.floor(Math.random() * 128 + 127); // 127-255
+//   const b = Math.floor(Math.random() * 128 + 127); // 127-255
+//   return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// function applyRandomPastelColorsToLinks() {
+//   const links = document.querySelectorAll("a");
+//   links.forEach((link) => {
+//     const pastelColor = getRandomPastelColor();
+//     link.style.color = pastelColor;
+//   });
+// }
+
 const observerCallback = (mutationList, observer) => {
   for (const mutation of mutationList) {
     if (mutation.type === "childList") {
       // 'chat_area' 클래스 내부의 'dl' 태그 안에 있는 모든 <img> 태그를 숨깁니다.
-      editCharAreaPadding(".chat_area dl dd");
+      editCharAreaPadding([".chat_area dl dd", ".chat_area dl dt"]);
 
       editCharAreaFlex(".chat_area dl");
 
       removeCharAreaImage([".chat_area dl img", ".chat_area dl span"]);
 
       removeElementBySelector(".pc");
+
+      moveDdTextToDt(".chat_area dl");
+
+      //   applyRandomPastelColorsToLinks(".chat_area dl a");
     }
   }
 };
@@ -48,13 +105,3 @@ const observer = new MutationObserver(observerCallback);
 
 // 옵저버가 전체 페이지의 변화를 관찰하도록 설정
 observer.observe(document.body, observerOptions);
-
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//   console.log(message);
-//   if (message.action === "toggle") {
-//     const elements = document.querySelectorAll(".chat_area dl img");
-//     elements.forEach((element) => {
-//       element.style.display = element.style.display === "none" ? "block" : "none";
-//     });
-//   }
-// });
