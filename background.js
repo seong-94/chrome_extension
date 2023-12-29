821bf65365a518d8886662df33c33a6902798d79
seong-94 <togussla@naver.com>
2023-12-29 21:19:46 +0900

    feat: 백그라운 로 메세지 보내는 기능 추가

--- background.js
+++ background.js
@@ -3,3 +3,28 @@
  * | 백그라운드 |
  * ---------------------------------------------------------------------------------
  **/
+// 초기 상태 설정
+chrome.storage.local.set({
+  applyRandomPastelColorsToNewLinks: false, // 초기 값을 false로 설정
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "toggle-applyRandomPastelColorsToNewLinks") {
+    chrome.storage.local.get("applyRandomPastelColorsToNewLinks", (data) => {
+      chrome.storage.local.set(
+        {
+          applyRandomPastelColorsToNewLinks: !data.applyRandomPastelColorsToNewLinks,
+        },
+        () => {
+          // 현재 활성 탭에 메시지를 전송합니다.
+          chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+              action: "update-applyRandomPastelColorsToNewLinks",
+              value: data.applyRandomPastelColorsToNewLinks,
+            });
+          });
+        }
+      );
+    });
+  }
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let functionWrapperCount = 4; // 생성하려는 function-wrapper 요소의 수
-  for (let i = 0; i < functionWrapperCount; i++) {
-    createFunctionWrapper(i);
+  // function-wrapper 요소를 생성합니다.
+  createFunctionWrapper(0);
+
+  // 생성된 function-wrapper 내부의 체크박스에 대한 참조를 가져옵니다.
+  const checkbox = document.getElementById("buttonId0");
+
+  // 체크박스가 존재하는지 확인하고, change 이벤트 리스너를 등록합니다.
+  if (checkbox) {
+    checkbox.addEventListener("change", function () {
+      chrome.runtime.sendMessage({
+        action: "toggle-applyRandomPastelColorsToNewLinks",
+      });
+    });
+  } else {
+    console.error("체크박스를 찾을 수 없습니다: #checkbox0");
   }
 });
 
@@ -9,11 +21,11 @@ function createFunctionWrapper(id) {
   // function-wrapper 생성
   let functionWrapper = document.createElement("div");
   functionWrapper.setAttribute("class", "function-wrapper");
-  functionWrapper.setAttribute("id", "buttonId" + id); // 개별 ID 부여
+  functionWrapper.setAttribute("id", "buttonId" + id);
 
   // 첫 번째 div 생성
   let div1 = document.createElement("div");
-  div1.textContent = "채팅창 닉네임 색상 적용 " + id; // 개별적인 설명 추가
+  div1.textContent = "채팅창 닉네임 색상 적용 ";
 
   // 두 번째 div 및 checkbox 생성
   let div2 = document.createElement("div");
@@ -21,20 +33,21 @@ function createFunctionWrapper(id) {
   let checkbox = document.createElement("input");
   checkbox.setAttribute("type", "checkbox");
   checkbox.setAttribute("class", "sc-gJwTLC ikxBAC");
-  checkbox.setAttribute("id", "checkbox" + id); // 개별적인 ID 부여
+  checkbox.setAttribute("id", "checkbox" + id);
+
+  // 설정 값을 조회하여 체크박스 상태를 결정합니다.
+  chrome.storage.local.get("applyRandomPastelColorsToNewLinks", function (data) {
+    if (data.applyRandomPastelColorsToNewLinks) {
+      checkbox.checked = true; // true일 때 체크합니다.
+    } else {
+      checkbox.checked = false; // false일 때 체크하지 않습니다.
+    }
+  });
+
   div2.appendChild(checkbox);
 
   functionWrapper.appendChild(div1);
   functionWrapper.appendChild(div2);
-
-  // checkbox에 change 이벤트 리스너 추가
-  checkbox.addEventListener("change", function () {
-    let message = this.checked ? "enable-color" : "disable-color";
-
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: message });
-    });
-  });
 
   // body에 function-wrapper 추가
   document.body.appendChild(functionWrapper);
